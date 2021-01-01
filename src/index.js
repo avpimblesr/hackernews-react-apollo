8084e967d03f32e7e2339e80821ce710f21c9615
avpimblesr <allan.pimble@gmail.com>
2021-01-01 16:36:48 -0500

    cloned the server from github

--- src/index.js
+++ src/index.js
@@ -3,9 +3,25 @@ import ReactDOM from 'react-dom';
 import './styles/index.css'
 import App from './components/App';
 
+import {
+  ApolloProvider,
+  ApolloProviderClient,
+  createHttpLink,
+  InMemoryCatch
+} from '@apollo/client';
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000'
+})
+
+const client = newApolloClient({
+  link: httpLink,
+  cache: new InMemoryCatch()
+})
+
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById('root')
 );
